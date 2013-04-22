8f358245162fb4665b76a45da72f13e6ecc819f2
nicohung <nicohung@umich.edu>
2013-04-22 10:00:04 -0400

    header update

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -14,13 +14,22 @@
 <?php
   $this->load->helper('html');
   echo "\n". link_tag('assets/css/664.css');
+  echo "\n". link_tag('assets/css/form.css');
+  echo "\n". link_tag('assets/css/news.css');
+  echo "\n". link_tag('assets/css/about.css');
+  echo "\n". link_tag('assets/css/custom-theme/jqueryui.css');
+
+  echo "\n". link_tag('assets/css/events.css');
+  echo "\n". link_tag('assets/css/jquery.timepicker.css');
+  echo "\n". link_tag('assets/css/chosen.css');
     // echo "\n". link_tag('assets/stylesheets/'. $theme .'.css');
   $this->load->helper('url');
 ?>
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 <script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.2/jquery-ui.min.js"></script>
+<script type="text/javascript" src="<?=base_url()?>assets/js/jquery.timepicker.min.js"></script>
+<script type="text/javascript" src="<?=base_url()?>assets/js/chosen.jquery.min.js"></script>
 <script type="text/javascript" src="../assets/js/html5gallery/html5gallery.js"></script>
-
 </head>
 
 <body>
@@ -31,11 +40,15 @@
   
   <div class="menu">
     <ul class="nav">
-      <li><a href="<?php echo site_url('home') ?>">Home</a></li>
+      <li><a href="<?php echo site_url() ?>">Home</a></li>
       <li><a href="<?php echo site_url('events') ?>">Upcoming Events</a></li>
       <li><a href="<?php echo site_url('news') ?>">News</a></li>
       <li><a href="<?php echo site_url('media') ?>">Media</a></li>
       <li><a href="<?php echo site_url('about') ?>">About</a></li>
       <li><a href="<?php echo site_url('contact') ?>">Contact</a></li>
+       <?php if($this->session->userdata('validated')){
+              $this->load->helper('url');
+              echo "<li><a href=\"".site_url('login_index')."\">Logout</a></li>";
+             }?>
     </ul> 
   </div>
